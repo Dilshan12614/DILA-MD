406f4b8e880f2fbb9247522f96c50928643a632d
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-10 07:44:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const {
   const path = require('path')
   const prefix = config.PREFIX
   
-  const ownerNumber = ['94773416478']
+  const ownerNumber = ['94740534738']
   
   const tempDir = path.join(os.tmpdir(), 'cache-temp')
   if (!fs.existsSync(tempDir)) {
