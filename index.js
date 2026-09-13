0eac5107451808d69f54c4866bd7ab3936c97ab0
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-13 16:11:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,8 +68,13 @@ const {
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
 const sessdata = config.SESSION_ID.replace("PRABATH-MD~", '');
-const cleanedId = sessdata.replace('PRABATH-MD~', '');
-const filer = File.fromURL('https://mega.nz' + cleanedId + '#');
+let megaUrl = sessdata;
+if (!megaUrl.startsWith('http')) {
+    let cleanedId = sessdata.replace('PRABATH-MD~', '');
+    megaUrl = 'https://mega.nz' + cleanedId + '#';
+}
+const filer = File.fromURL(megaUrl);
+
 
 filer.download((err, data) => {
 if(err) throw err
