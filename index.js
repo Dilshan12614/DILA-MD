2024321a520b9a55abc942afdc18c2714d623590
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-13 22:50:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const {
   const path = require('path')
   const prefix = config.PREFIX
   
-  const ownerNumber = ['94740534738']
+  const ownerNumber = ['94773416478']
   
   const tempDir = path.join(os.tmpdir(), 'cache-temp')
   if (!fs.existsSync(tempDir)) {
@@ -67,15 +67,8 @@ const {
   //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID.replace("PRABATH-MD~", '');
-let megaUrl = sessdata;
-if (!megaUrl.startsWith('http')) {
-    let cleanedId = sessdata.replace('PRABATH-MD~', '');
-    megaUrl = 'https://mega.nz' + cleanedId + '#';
-}
-const filer = File.fromURL(megaUrl);
-
-
+const sessdata = config.SESSION_ID.replace("LUXALGO=", '');
+const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
 fs.writeFile(__dirname + '/sessions/creds.json', data, () => {
