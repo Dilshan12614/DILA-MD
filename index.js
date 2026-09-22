02120926b432d63da7172f1022d8ed1bb74fabec
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-22 19:59:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -172,8 +172,15 @@ const port = process.env.PORT || 8000;
   const content = JSON.stringify(mek.message)
   const from = mek.key.remoteJid
   const quoted = type == 'extendedTextMessage' && mek.message.extendedTextMessage.contextInfo != null ? mek.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
-  const body = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : (type == 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : ''
-  const isCmd = body.startsWith(prefix)
+  const body = (type === 'conversation') ? mek.message.conversation : 
+             (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : 
+             (type === 'imageMessage') && mek.message.imageMessage.caption ? mek.message.imageMessage.caption : 
+             (type === 'videoMessage') && mek.message.videoMessage.caption ? mek.message.videoMessage.caption : 
+             (type === 'buttonsResponseMessage') ? mek.message.buttonsResponseMessage.selectedButtonId : 
+             (type === 'templateButtonReplyMessage') ? mek.message.templateButtonReplyMessage.selectedId : '';
+
+const isCmd = body.startsWith(prefix);
+
   var budy = typeof mek.text == 'string' ? mek.text : false;
   const command = isCmd ? body.slice(prefix.length).trim().split(' ').shift().toLowerCase() : ''
   const args = body.trim().split(/ +/).slice(1)
